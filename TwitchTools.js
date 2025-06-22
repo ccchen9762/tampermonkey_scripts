389835688a7d1799c4b7711b7672e79d0f4f9b57
ccchen <ccchen9762@gmail.com>
2025-06-22 00:20:00 -0700

    Run the portrait mode function on page loaded; Fix chat height sometimes go over the screen;

--- TwitchTools.js
+++ TwitchTools.js
@@ -42,23 +42,25 @@
     /* ========== Move chat under stream and hide info section when window becomes portrait mode ========== */
     /* grid-template-columns: left column's width, middle takes remaining, right column's width */
     const reorderCSS = `
-        .main-container {
-            display: grid !important;
-            grid-template-columns: auto 1fr;
-            grid-template-rows: auto 1fr;
-        }
-        .left-col {
-            grid-column: 1;
-            grid-row: 1 / span 2;
-        }
-        .middle-col {
-            grid-column: 2;
-            grid-row: 1;
-        }
-        .right-col {
-            grid-column: 2;
-            grid-row: 2;
-            width: 100% !important;
+         @media (max-aspect-ratio: 1/1) {
+            .main-container {
+                display: grid !important;
+                grid-template-columns: auto 1fr;
+                grid-template-rows: auto 1fr;
+            }
+            .left-col {
+                grid-column: 1;
+                grid-row: 1 / span 2;
+            }
+            .middle-col {
+                grid-column: 2;
+                grid-row: 1;
+            }
+            .right-col {
+                grid-column: 2;
+                grid-row: 2;
+                width: 100% !important;
+            }
         }
         .info-div {
             display: none !important;
@@ -73,31 +75,27 @@
     reorderStyle.appendChild(document.createTextNode(reorderCSS));
     document.head.appendChild(reorderStyle);
 
+    const rootDiv = document.getElementById('root');
+    if(!rootDiv){
+        console.log('(Twitch tool) [Error] Channel info not found');
+    }
+
+    const mainContainer = rootDiv.children[0].children[0].children[1];
+
+    const leftCol = mainContainer.children[0];
+    const middleCol = mainContainer.children[1];
+    const rightCol = mainContainer.children[2];
+
+    mainContainer.classList.add('main-container');
+    leftCol.classList.add('left-col');
+    middleCol.classList.add('middle-col');
+    rightCol.classList.add('right-col');
+
     let portraitMode = false;
     let chatOffset = null; // store the chat initial position style offset
 
-    // tab switching etc.
-    window.addEventListener('visibilitychange', function(e) {
-        console.log('(Twitch tool) [Info] Visibility changed');
-        checkWindowRatio();
-    });
-
-    window.addEventListener('resize', function(e) {
-        console.log('(Twitch tool) [Info] Resized');
-        checkWindowRatio();
-    });
-
     async function checkWindowRatio() {
-        const mainContainer = document.getElementById('root').children[0].children[0].children[1];
 
-        if(!mainContainer){
-            console.log('(Twitch tool) [Error] Main container not found');
-            return;
-        }
-
-        const leftCol = mainContainer.children[0];
-        const middleCol = mainContainer.children[1];
-        const rightCol = mainContainer.children[2];
         const infoDiv = document.getElementsByClassName('channel-info-content')[0].children[1];
         const chatDiv = document.getElementsByClassName('channel-root__right-column')[0];
 
@@ -121,23 +119,19 @@
                 const collapseButton = document.querySelector('button[aria-label="Collapse Chat"]');
                 if(collapseButton){
                     collapseButton.click();
-
-                    await new Promise(resolve => setTimeout(resolve, 100)); // wait for layout collapse
-
-                    // Keeps the chat visible while in collapse status
-                    rightCol.children[0].classList.remove('ieZICN');
-                    rightCol.children[0].classList.add('eOzPrw');
-
-                    rightCol.children[0].children[1].classList.remove('iLMtDH');
-                    rightCol.children[0].children[1].classList.add('gYVLVA');
                 }
+
+                await new Promise(resolve => setTimeout(resolve, 100)); // wait for layout collapse
+
+                // Keeps the chat visible while in collapse status
+                rightCol.children[0].classList.remove('ieZICN');
+                rightCol.children[0].classList.add('eOzPrw');
+
+                rightCol.children[0].children[1].classList.remove('iLMtDH');
+                rightCol.children[0].children[1].classList.add('gYVLVA');
 
                 await new Promise(resolve => setTimeout(resolve, 100));
 
-                mainContainer.classList.add('main-container');
-                leftCol.classList.add('left-col');
-                middleCol.classList.add('middle-col');
-                rightCol.classList.add('right-col');
                 infoDiv.classList.add('info-div');
                 chatDiv.classList.add('chat-div', 'channel-root__right-column--expanded');
                 chatDiv.style.setProperty('transform', 'none');
@@ -145,14 +139,38 @@
                 portraitMode = true;
             }
 
+            const topNav = document.getElementsByClassName('top-nav')[0];
+            const streamElement = document.getElementsByClassName('persistent-player')[0];
+            const streamInfoElement = document.getElementById('live-channel-stream-information');
+            const chatShellElement = document.getElementsByClassName('chat-shell')[0];
+
+            if(!topNav){
+                console.log('(Twitch tool) [Error] Top nav not found');
+                return;
+            }
+            if(!streamElement){
+                console.log('(Twitch tool) [Error] Stream not found');
+                return;
+            }
+            if(!streamInfoElement){
+                console.log('(Twitch tool) [Error] Stream info not found');
+                return;
+            }
+            if(!chatShellElement){
+                console.log('(Twitch tool) [Error] Chat shell not found');
+                return;
+            }
+
+            const topNavHeight = topNav.offsetHeight;
+            const streamHeight = streamElement.offsetHeight;
+            const streamInfoHeight = streamInfoElement.offsetHeight;
+
+            chatShellElement.style.setProperty('height', `calc(100vh - ${topNavHeight}px - ${streamHeight}px - ${streamInfoHeight}px)`, 'important');
+
             console.log('(Twitch tool) [Info] Chat switched to portrait mode');
         }
         else if (windowRatio >= 1) {
             if(portraitMode){
-                mainContainer.classList.remove('main-container');
-                leftCol.classList.remove('left-col');
-                middleCol.classList.remove('middle-col');
-                rightCol.classList.remove('right-col');
                 infoDiv.classList.remove('info-div');
                 chatDiv.classList.remove('chat-div');
                 chatDiv.style.setProperty('transform', chatOffset);
@@ -168,8 +186,33 @@
                 portraitMode = false;
             }
 
+            const chatShellElement = document.getElementsByClassName('chat-shell')[0];
+            chatShellElement.style.setProperty('height', '100%');
+
             console.log('(Twitch tool) [Info] Chat restored to landscape mode');
         }
+    }
+
+    // tab switching etc.
+    window.addEventListener('visibilitychange', function(e) {
+        console.log('(Twitch tool) [Info] Visibility changed');
+        checkWindowRatio();
+    });
+
+    window.addEventListener('resize', function(e) {
+        console.log('(Twitch tool) [Info] Resized');
+        checkWindowRatio();
+    });
+
+    if (document.readyState === 'loading') {
+        // If the DOM is not yet loaded, wait for DOMContentLoaded
+        document.addEventListener('DOMContentLoaded', function(e) {
+        console.log('(Twitch tool) [Info] DOMContentLoaded.');
+        setTimeout(checkWindowRatio, 500);
+    });
+    } else {
+        console.log('(Twitch tool) [Info] Document already loaded.');
+        setTimeout(checkWindowRatio, 500);
     }
 
     console.log('(Twitch tool) [Info] Script started.');
